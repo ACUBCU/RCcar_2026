940ea24ad076837f949d0e42f3b95a659f9f8e2e
ldh <ldh9632@gmail.com>
2026-06-23 09:30:28 +0900

    스케줄 MD파일 추가

--- python/code/week1/0622.py
+++ python/code/week1/0622.py
@@ -1,11 +1,3 @@
-# 1교시: 수업 오리엔테이션
-# 2교시: wsl2 설치, VsCode 설치
-# 3교시: git 설치, github 계정 만들기, github repository 만들기
-# 4교시: Python 기초
-# 5교시: print 함수 해석
-# 6교시: 변수와 자료형
-# 7교시: 연산자와 표현식, input 함수 해석
-# 8교시: 문자열 f-string, format
 class A:
     def __repr__(self):
         return "a"
