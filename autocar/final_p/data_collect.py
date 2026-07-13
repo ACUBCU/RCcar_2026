d1fc74fad31939582944d1025450387ee5c5078d
ldh <ldh9632@gmail.com>
2026-07-13 13:38:16 +0900

    데이터 수집

--- autocar/final_p/data_collect.py
+++ autocar/final_p/data_collect.py
@@ -1,5 +1,9 @@
 # 주행 불가능 영역을 탐지하기 위한 코드
 from pop import Pilot
+car = Pilot.AutoCar()
+
+car.camPan(85) #카메라 좌우 정중앙
+car.camTilt(-17) # 카메라 상하 아래 최대로
 
 cam = Pilot.Camera(width=300, height=300)
 cam.show()
